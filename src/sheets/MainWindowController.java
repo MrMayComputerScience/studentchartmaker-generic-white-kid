acf503fbb82d6a6b5066872f4a7e5b4d223f2bba
Cshauli17 <cshaulis.baseball17@gmail.com>
2017-10-05 13:58:27 -0500

    started file Reading
    not complete

--- src/sheets/MainWindowController.java
+++ src/sheets/MainWindowController.java
@@ -1,4 +1,23 @@
 package sheets;
 
+import javafx.fxml.FXML;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class MainWindowController {
+    @FXML private String fileName;
+    public void readFile(){
+        try{
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bf = new BufferedReader(fileReader);
+            while(()){
+
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Unable to open file \"" + fileName + "\"");
+        }
+    }
 }
