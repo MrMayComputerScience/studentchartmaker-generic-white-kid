1a25e32d9b73e216c39622208e3f41983d5925af
Mason McCully <mmccully2000@gmail.com>
2017-10-05 14:06:24 -0500

    Merge changes?
    It keeps adding <<<<HEAD to literally everything??

--- src/sheets/MainWindowController.java
+++ src/sheets/MainWindowController.java
@@ -2,31 +2,28 @@ package sheets;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-
-public class MainWindowController {
-
-    @FXML
-    public void printSpreadsheet(){
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("spreadsheet.fxml"));
-        SpreadsheetController controller = loader.getController();
-        controller.print();
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class MainWindowController {
-    @FXML private String fileName;
-    public void readFile(){
-        try{
+    @FXML
+    private String fileName;
+
+    @FXML
+    public void printSpreadsheet() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("spreadsheet.fxml"));
+        SpreadsheetController controller = loader.getController();
+        controller.print();
+    }
+
+    public void readFile() {
+        try {
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bf = new BufferedReader(fileReader);
-            while(()){
-
-            }
-        }
-        catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("Unable to open file \"" + fileName + "\"");
         }
+
     }
 }
