4b007d0b99391014a410a095a4b5708700b8ec8d
Mason McCully <mmccully2000@gmail.com>
2017-10-05 13:21:50 -0500

    Barebones commit
    generally most of the basic stuff we need

---
+++ src/SpreadsheetController.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.print.Printer;
+import javafx.print.PrinterJob;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+public class SpreadsheetController {
+    @FXML private GridPane pane;
+    @FXML public void initialize(){
+        Printer printer = null;
+        for(Printer p : Printer.getAllPrinters()){
+            System.out.println(p.getName());
+            if(p.getName().contains("XPS"))
+                printer = p;
+        }
+        PrinterJob pj = PrinterJob.createPrinterJob();
+        pj.setPrinter(printer);
+        Stage popup = new Stage();
+        pj.showPageSetupDialog(popup);
+        pj.showPrintDialog(popup);
+
+        pj.printPage(pane);
+        pj.endJob();
+        Platform.exit();
+    }
+}
