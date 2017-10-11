90e8bbad3e76a1f5bfe73f3f80c9a81fcbde15aa
Mason McCully <mmccully2000@gmail.com>
2017-10-11 13:56:24 -0500

    Added getFileContents and helper

--- src/sheets/MainWindowController.java
+++ src/sheets/MainWindowController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainWindowController {
     @FXML private String fileName;
@@ -37,26 +39,35 @@ public class MainWindowController {
         SpreadsheetController controller = loader.getController();
         controller.print();
     }
-    ///First value is students text, second = columns, third = header
+    ///Order of values is always students, columns, header
     public String[] getFileContents(String[] fileNameArr){
-        String[] arr = new String[3];
-        arr[0] = readFileHelper(fileNameArr[0]);
-        arr[1] = readFileHelper(fileNameArr[1]);
-        arr[2] = readFileHelper(fileNameArr[2]);
-        return arr;
+        String[] contents = new String[fileNameArr.length];
+        for(int i = 0; i < contents.length; i++){
+            contents[i] = readFileHelper(fileNameArr[i]);
+        }
+        return contents;
     }
     public String readFileHelper(String fileName){
         String contents = "";
         File file = new File(fileName);
-
+        try(FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr)){
+            while(br.readLine() != null){
+                contents += br.readLine() + "\n";
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
         return contents;
     }
-    ///Returns an array where the first value is the students.txt name, second is the columns.txt, and 3rd is header.txt
+    ///Returns an array where the order of values is always students.txt, columns.txt, header.txt
     public String[] readFirstFile(){
         String line = null;
         String studentFile;
         String columnFile;
         String headerFile;
+        List<String> list = new LinkedList<String>();
         String[] fileArray = new String[3];
         //TODO: Add file chooser
         fileName = fileField.getText();
@@ -66,28 +77,39 @@ public class MainWindowController {
         try{
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bf = new BufferedReader(fileReader);
+            int count = 0;
             while((line = bf.readLine()) != null){
                 if(line.contains("Students =")){
                     studentFile = line;
                     String[] studentFileSplit = studentFile.split("= ");
                     studentFile = studentFileSplit[1];
                     fileArray[0] = studentFile;
+                    count++;
                 }
                 else if(line.contains("Columns =")){
                     columnFile = line;
                     String[] columnFileSplit = columnFile.split("= ");
                     columnFile = columnFileSplit[1];
                     fileArray[1] = columnFile;
+                    count++;
                 }
                 else if(line.contains("Header =")){
                     headerFile = line;
                     String[] headerFileSplit = headerFile.split("= ");
                     headerFile = headerFileSplit[1];
                     fileArray[2] = headerFile;
+                    count++;
+                }
+                else if(count == 3){
+                    for(String s : fileArray){
+                        list.add(s);
+                    }
+                    count = 0;
                 }
                 else{
                     System.err.println("FILE NOT FORMATTED PROPERLY");
                 }
+
 
             }
         }
@@ -96,6 +118,9 @@ public class MainWindowController {
         }
         catch(IOException e){
             System.out.println("Error reading file \"" + fileName + "\"");
+        }
+        if(list.size() > 0){
+            list.toArray(fileArray);
         }
         return fileArray;
     }
