308bf73ad1fcfb036a3a3c28add332fdcc5968af
Mason McCully <mmccully2000@gmail.com>
2017-10-26 08:42:00 -0500

    This is all shit

--- src/sheets/PrintToHtml.java
+++ src/sheets/PrintToHtml.java
@@ -8,28 +8,39 @@ public class PrintToHtml {
         html = new File(fileName);
     }
     boolean addParameterToElement(String parameter, String value, String id){
+        boolean success = false;
         try(
                 FileReader fis = new FileReader(html);
                 BufferedReader in = new BufferedReader(fis);
                 FileOutputStream fos = new FileOutputStream(html);
                 PrintWriter out = new PrintWriter(fos)
         ){
+
+            String toAdd = parameter + "=\'" + value + "\' ";
             String line = "";
-            int currentLine = 0;
+            String newDoc = "";
             while((line = in.readLine()) != null){
-                currentLine++;
                 int index = line.indexOf("id=\""+id+"\"");
                 if(index >= 0){
-                    index--;
-
+                    String before = line.substring(0,index);
+                    String after = line.substring(index, line.length());
+                    String newLine = before + toAdd + after;
+                    newDoc += newLine + "\n";
+                    success = true;
                 }
+                else
+                    newDoc += line + "\n";
             }
+            out.write(newDoc);
+
         }
         catch(FileNotFoundException e){
-
+            System.err.println("HTML File not found: "+html.getAbsolutePath());
+            e.printStackTrace();
         }
         catch(IOException e){
-
+            e.printStackTrace();
         }
+        return success;
     }
 }
